e9fbffd180ceb75cd18429c27ce51375040064b4
csoc-ut <63271121+csoc-ut@users.noreply.github.com>
2020-04-07 01:42:14 -0500

    Initial Navbar Work

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,8 @@ function open_chapter(n, book_name) {
   sessionStorage.setItem('chapter_number', n);
   window.location = book_name;
 }
+
+function navbar_scroll_offset() {
+  window.scrollBy(0, -80);
+}
+window.onload = navbar_scroll_offset;
