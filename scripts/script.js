aa9572b976022099a32e7a30d27df0789e2eeefa
csoc-ut <63271121+csoc-ut@users.noreply.github.com>
2020-04-18 22:55:34 -0500

    Added Day of Week to Reading Schedule

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 function open_reading(reading) {
-    sessionStorage.setItem('reading', reading.textContent);
+    sessionStorage.setItem('reading', reading.getElementsByTagName('p')[0].innerHTML);
+    // console.log(reading.getElementsByTagName('p')[0].innerHTML);
     window.location.href='reading_plan/todays_reading.html';
 }
 
